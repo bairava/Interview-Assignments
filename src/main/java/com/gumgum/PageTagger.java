0faf283ce52718a77055235d623b591dc31648fc
C H Bairava Raman <bairavaramanch@gmail.com>
2016-07-11 22:41:57 -0700

    Code changes to tag text from entered URL (String input)

--- src/main/java/com/gumgum/PageTagger.java
+++ src/main/java/com/gumgum/PageTagger.java
@@ -1,4 +1,11 @@
 package com.gumgum;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 /**
  * @author Bairava Raman
@@ -6,16 +13,28 @@ import edu.stanford.nlp.tagger.maxent.MaxentTagger;
  * Module to perform text tagging from the entered URL
 **/
 public class PageTagger {
-//Tagger Model used
+
 static final String TAGGER_MODEL ="english-left3words-distsim.tagger";
 MaxentTagger maxTagger = null;
-
-public static void main(String[] args) {
+private Document htmlDocument;
+/**
+ * Main method
+ * @param The URL String passed as argument
+ * @return void
+ **/
+public static void main(String[] args) throws MalformedURLException {
 		PageTagger pageTaggerObject = new PageTagger();
-			String sample = "Testing the tagger module";
-			System.out.println(pageTaggerObject.tagText(sample));
+		URL input = null;
+		try {
+			input = new URL(args[0]);
+			String sample = pageTaggerObject.getText(input);
+			PrintWriter printWriter = new PrintWriter("Tagged_Output.txt");
+			printWriter.write(pageTaggerObject.tagText(sample));
+			printWriter.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-
 /**
  * Method to tag the text
  * @param String
@@ -25,5 +44,18 @@ public String tagText(String inputText){
 	maxTagger = new MaxentTagger(TAGGER_MODEL);
 	String result = maxTagger.tagString(inputText);
 	return result;
-	} 
+} 
+/**
+ * Method to retrieve the plain text from given URL
+ * @param URL
+ * @return String
+ **/
+public String getText(URL inputURL) throws IOException{
+	Connection connection = Jsoup.connect(inputURL.toString()).maxBodySize(Integer.MAX_VALUE).userAgent("Chrome/13.0.782.112");
+	htmlDocument = connection.get();
+	ContentExtractor extractor = new ContentExtractor();
+	StringBuilder text= new StringBuilder(extractor.extractFromDocument(htmlDocument));
+	String response = text.toString();
+	return response;
+	}
 }
